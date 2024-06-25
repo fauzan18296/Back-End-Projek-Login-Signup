7c1d1a2f06242534ad9a49426267b5e965dfa0fc
Ahmad Fauzan <fauzan18296@gmail.com>
2024-06-26 00:08:17 +0700

    message: program ini masih belum fix dan masih ada error limit

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import express from 'express';
 import db from './config.js'
 import bodyParser from 'body-parser';
 const app = express();
-const encoder = bodyParser.urlencoded({extended:true})
+
 // use body-parser
-app.use(encoder)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 // static file
 app.use(express.static('public'));
 // template engine set ejs template
@@ -18,17 +19,23 @@ app.get('/signup', (req, res) => {
 res.render('signup');
 })
 
-app.post('/signup',(req,res) => {
+app.post('/signup',async (req,res) => {
   const data = {
-    name:req.body.username,
-    password:req.body.password
+    name:await req.body.username,
+    password:await req.body.password
   }
- db.query(`INSERT INTO users(username, password) VALUES('${data.name}', '${data.password}')`,(error) => {
-    if(error)throw error
-    res.redirect('/')
-    res.end()
-  })
- })
+// Membuat query SELECT * FROM users WHERE username AND password agar tidak ada duplicate username dan password
+db.query(`SELECT * FROM users WHERE username = '${data.name}' AND password = '${data.password}' `,(err,results) => {
+  if(err)throw Error;
+  if(results.length > 0) res.status(409).json({Message: "Nama pengguna sudah ada!"})
+
+    // Membuat query INSERT untuk menambahkan username dan password jika tidak ada isi yang sama atau duplicate
+db.query(`INSERT INTO users(username, password) VALUES('${data.name}', '${data.password}')`,(error) => {
+  if(error)throw Error;
+  res.redirect("/")
+  })//end query INSERT
+})//end query SELECT * FROM users WHERE username AND password 
+  })//end line http method post
 
 const port = 3000;
 app.listen(port, () => {
